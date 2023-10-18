3ecaa2cdab3fe0afd62cc93db4cc3be1c9e8d190
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-18 02:58:41 -0600

    atobb

--- views/encapsulamiento/actividadesEncapsulamiento/actividad1/prueba.js
+++ views/encapsulamiento/actividadesEncapsulamiento/actividad1/prueba.js
@@ -1,27 +1,5 @@
-// Realizar una solicitud AJAX para obtener los datos del archivo.php
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            const actData = JSON.parse(xhr.responseText);
-            // Aquí puedes acceder a las variables recuperadas
-            const aciertos = actData.aciertos;
-            const actRealizada = actData.actRealizada;
-            const fechaEntrega = actData.fechaEntrega;
-            const alumnoID = actData.alumnoID;
-            const alumnoUsername = actData.alumnoUsername;
-
-            // Haz lo que necesites con las variables
-            console.log('Aciertos:', aciertos);
-            console.log('Actividad Realizada:', actRealizada);
-            console.log('Fecha de Entrega:', fechaEntrega);
-            console.log('ID del Alumno:', alumnoID);
-            console.log('Nombre de Usuario del Alumno:', alumnoUsername);
-        } else {
-            console.error('Hubo un error al obtener los datos:', xhr.status, xhr.statusText);
-        }
-    }
-};
-
-xhr.open('GET', './control.php', true);
-xhr.send();
+console.log("Aciertos:", actData.aciertos);
+console.log("Actividad realizada:", actData.actRealizada);
+console.log("Fecha de entrega:", actData.fechaEntrega);
+console.log("ID del alumno:", actData.alumnoID);
+console.log("Nombre de usuario del alumno:", actData.alumnoUsername);
