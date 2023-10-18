35a137acdb360535ecfc6e915f7d7e8549240825
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-18 04:08:54 -0600

    asd

--- views/encapsulamiento/actividadesEncapsulamiento/actividad1/validarActividad.js
+++ views/encapsulamiento/actividadesEncapsulamiento/actividad1/validarActividad.js
@@ -1,31 +1,23 @@
-/*Pasar las variables de PHP a JS
-Fuente: https://www.youtube.com/watch?v=ev9wASpNN9A
-*/
+/*Control de vistas para las actividades*/
 var xhr = new XMLHttpRequest();
-var aciertos, actRealizada, fechaEntrega, alumnoID, alumnoUsername;
+var actRealizada;
 
 xhr.open("GET", "control.php", false); //true para ejecutar de manera asincrona y false para hacerlo de manera asincrona
 xhr.onload = function () {
   if (xhr.status === 200) {
     //var json = xhr.responseText;
     var json = JSON.parse(xhr.responseText);
-    aciertos = json.aciertos;
     actRealizada = json.actRealizada;
-    fechaEntrega = json.fechaEntrega;
-    alumnoID = json.alumnoID;
-    alumnoUsername = json.alumnoUsername;
   } else {
     console.log("Error al cargar el servidor");
   }
 };
 xhr.send();
 
-console.log(aciertos);
-console.log(actRealizada);
-console.log(fechaEntrega);
-console.log(alumnoID);
-console.log(alumnoUsername);
-
 if (actRealizada === 'Si') {
   window.location.href = "retroalimentacion.html";
+}
+
+if (actRealizada === 'No') {
+  window.location.href = "cuestionario.html";
 }
