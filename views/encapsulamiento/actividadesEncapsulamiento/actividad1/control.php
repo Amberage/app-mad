3fd19c18e0351a945146cd59b330bc7b58fd5967
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-18 01:45:37 -0600

    PHP 1

--- views/encapsulamiento/actividadesEncapsulamiento/actividad1/control.php
+++ views/encapsulamiento/actividadesEncapsulamiento/actividad1/control.php
@@ -1,59 +1,29 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/php/config.php';
+/* include $_SERVER['DOCUMENT_ROOT'] . '/php/config.php'; */
 
+$servername = "localhost";
+$username = "amberage_root";
+$password = "Q2om%)?H.sAQV(r(MD";
+$dbname = "amberage_madness";
 
-/* $conn = new mysqli($servername, $username, $password, $dbname);
+// recuperar datos del usuario
+if (isset($_COOKIE['userData'])) {
+    // Decodifica y deserializa la información de la cookie
+    $userDataJSONEncoded = $_COOKIE['userData'];
+    $userDataJSON = base64_decode($userDataJSONEncoded);
+    $userData = json_decode($userDataJSON, true); // true para obtener un array asociativo
 
-if ($conn->connect_error) {
-    echo json_encode(array("error" => true, "message" => "Muerte 1"));
-    die(json_encode(array("error" => true, "message" => "Error de conexión: " . $conn->connect_error)));
-}
+    // Ahora puedes acceder a 'username' e 'id' almacenados en la cookie
+    $alumnoID = $userData['id'];
+    $alumnoUsername = $userData['username'];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$sql = "SELECT * FROM usuarios WHERE username='$username'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $hashedPassword = $row['password'];
-
-    // Obtén la IP del usuario
-    $ip = $_SERVER['REMOTE_ADDR'];
-
-    // Suponiendo que $userData es un array con los datos del usuario que quieres almacenar en la cookie
-    $userData = array(
-        "id" => $row['id'],
-        "username" => $row['username'],
-        "nombre" => $row['nombre'],
-        "aPaterno" => $row['aPaterno'],
-        "aMaterno" => $row['aMaterno'],
-        "correo" => $row['correo'],
-        "userType" => $row['userType'],
-        "ip" => $ip,
-        "fechaRegistro" => $row['fechaRegistro']
-    );
-
-    // Convertir el array a JSON y codificar para que sea seguro para las cookies
-    $userDataJSON = json_encode($userData);
-    $userDataJSONEncoded = base64_encode($userDataJSON);
-
-    // Establecer la cookie con la información del usuario
-    setcookie("userData", $userDataJSONEncoded, time() + (3600), "/"); 
-
-    // Verificar la contraseña proporcionada con la contraseña encriptada
-    if (password_verify($password, $hashedPassword)) {
-        // Contraseña válida, inicio de sesión exitoso
-        echo json_encode(array("error" => false, "message" => "Inicio de sesión exitoso", "redirect" => "home.html"));
-    } else {
-        // Contraseña incorrecta
-        echo json_encode(array("error" => true, "message" => "Contraseña Incorrecta"));
-    }
+    // Puedes usar estas variables como necesites en tu código
+    echo "Username: " . $usernameFromCookie . "<br>";
+    echo "ID: " . $idFromCookie;
 } else {
-    // Usuario no encontrado
-    echo json_encode(array("error" => true, "message" => "Usuario inexistente"));
+    header("Location: " . $_SERVER['DOCUMENT_ROOT'] . "/index.html");
+    exit();  // Asegúrate de salir del script después de la redirección
 }
 
-$conn->close(); */
+
 ?>
