bc2155a262f83aa698a4bceccbed4a79f9ae517c
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-18 03:30:33 -0600

    test

--- views/encapsulamiento/actividadesEncapsulamiento/actividad1/prueba.js
+++ views/encapsulamiento/actividadesEncapsulamiento/actividad1/prueba.js
@@ -1,11 +1,23 @@
 var xhr = new XMLHttpRequest();
+var aciertos, actRealizada, fechaEntrega, alumnoID, alumnoUsername;
+
 xhr.open('GET', 'control.php');
 xhr.onload = function() {
     if(xhr.status === 200) {
         var json = xhr.responseText;
-        console.log(json);
+        var aciertos = json[0].aciertos;
+        var actRealizada = json[0].actRealizada;
+        var fechaEntrega = json[0].fechaEntrega;
+        var alumnoID = json[0].alumnoID;
+        var alumnoUsername = json[0].alumnoUsername;
     } else {
         console.log("Error al cargar el servidor");
     }
 }
 xhr.send();
+
+console.log(aciertos);
+console.log(actRealizada);
+console.log(fechaEntrega);
+console.log(alumnoID);
+console.log(alumnoUsername);
