c07d7746054718f171d856609286bd8a6033b5a2
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-18 03:32:07 -0600

    a

--- views/encapsulamiento/actividadesEncapsulamiento/actividad1/prueba.js
+++ views/encapsulamiento/actividadesEncapsulamiento/actividad1/prueba.js
@@ -1,3 +1,6 @@
+/*Pasar las variables de PHP a JS
+Fuente: https://www.youtube.com/watch?v=ev9wASpNN9A
+*/
 var xhr = new XMLHttpRequest();
 var aciertos, actRealizada, fechaEntrega, alumnoID, alumnoUsername;
 
@@ -10,14 +13,20 @@ xhr.onload = function() {
         var fechaEntrega = json[0].fechaEntrega;
         var alumnoID = json[0].alumnoID;
         var alumnoUsername = json[0].alumnoUsername;
+
+        console.log(aciertos);
+        console.log(actRealizada);
+        console.log(fechaEntrega);
+        console.log(alumnoID);
+        console.log(alumnoUsername);
     } else {
         console.log("Error al cargar el servidor");
     }
 }
 xhr.send();
 
-console.log(aciertos);
-console.log(actRealizada);
-console.log(fechaEntrega);
-console.log(alumnoID);
-console.log(alumnoUsername);
+console.log("1"+aciertos);
+console.log("1"+actRealizada);
+console.log("1"+fechaEntrega);
+console.log("1"+alumnoID);
+console.log("1"+alumnoUsername);
