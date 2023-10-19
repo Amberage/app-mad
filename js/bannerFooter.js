0c87ced770898c4bb50c6a4e48ad6b0da501e4fb
MarianaVSilva <marianavazquezsilva@gmail.com>
2023-10-19 01:09:27 -0600

    Header

--- js/bannerFooter.js
+++ js/bannerFooter.js
@@ -2,7 +2,7 @@
 En caso de querer realizar cambios al header o footer se deben realizar en los codigos de abajo en la seccción del HTML
 */
 
-// Header
+// Headers
 const menuHTML = `
   <div class="menu">
     <ul>
@@ -10,14 +10,63 @@ const menuHTML = `
       <li></li>
       <li><a href="/views/encapsulamiento/encapsulamiento.html">Encapsulamiento</a></li>
       <li></li>
-      <li><a href="#">Herencia</a></li>
+      <li><a href="/views/herencia/herencia.html">Herencia</a></li>
       <li></li>
-      <li><a href="#">Polimorfismo</a></li>
+      <li><a href="/views/polimorfismo/polimorfismo.html">Polimorfismo</a></li>
       <li></li>
       <li><a href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>
     </ul>
   </div>
 `;
+
+const menuHTLMEncap = `
+  <div class="menu">
+    <ul>
+      <li><a href="/views/home.html">Inicio</a></li>
+      <li></li>
+      <li><a href="/views/encapsulamiento/encapsulamiento.html" class="menu-destacado">Encapsulamiento</a></li>
+      <li></li>
+      <li><a href="/views/herencia/herencia.html">Herencia</a></li>
+      <li></li>
+      <li><a href="/views/polimorfismo/polimorfismo.html">Polimorfismo</a></li>
+      <li></li>
+      <li><a href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>
+    </ul>
+  </div>
+`;
+
+const menuHTLMHer = `
+  <div class="menu">
+    <ul>
+      <li><a href="/views/home.html">Inicio</a></li>
+      <li></li>
+      <li><a href="/views/encapsulamiento/encapsulamiento.html">Encapsulamiento</a></li>
+      <li></li>
+      <li><a href="/views/herencia/herencia.html" class="menu-destacado">Herencia</a></li>
+      <li></li>
+      <li><a href="/views/polimorfismo/polimorfismo.html">Polimorfismo</a></li>
+      <li></li>
+      <li><a href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>
+    </ul>
+  </div>
+`;
+
+const menuHTLMPoli = `
+  <div class="menu">
+    <ul>
+      <li><a href="/views/home.html">Inicio</a></li>
+      <li></li>
+      <li><a href="/views/encapsulamiento/encapsulamiento.html">Encapsulamiento</a></li>
+      <li></li>
+      <li><a href="/views/herencia/herencia.html">Herencia</a></li>
+      <li></li>
+      <li><a href="/views/polimorfismo/polimorfismo.html" class="menu-destacado">Polimorfismo</a></li>
+      <li></li>
+      <li><a href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>
+    </ul>
+  </div>
+`;
+
 
 const menuContainer = document.getElementById('menuHeader'); // Busca en el HTML el id menuHeader para vacciar el menuHTML
 menuContainer.innerHTML = menuHTML;
