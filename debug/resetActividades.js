aa0869afca873778863a9fe22445cae4615b27bb
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-22 14:57:22 -0600

    Ajustes de debug

--- debug/resetActividades.js
+++ debug/resetActividades.js
@@ -2,7 +2,22 @@ function resetEncapsulamiento() {
     var xhr = new XMLHttpRequest();
     var serverMsg;
 
-    xhr.open("GET", "/debug/resetActividades.php", false);
+    xhr.open("GET", "/debug/resetEncapsulamiento.php", false);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            alert(JSON.stringify(json));
+        } else {
+            console.log("Error al cargar el servidor");
+        }
+    };
+    xhr.send();
+}
+function resetHerencica() {
+    var xhr = new XMLHttpRequest();
+    var serverMsg;
+
+    xhr.open("GET", "/debug/resetHerencia.php", false);
     xhr.onload = function () {
         if (xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
