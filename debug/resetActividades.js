b2359b23626e2ee1e8f89a6eaa02534426bdb69b
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-22 22:49:33 -0600

    Reset Polimorfismo

--- debug/resetActividades.js
+++ debug/resetActividades.js
@@ -31,3 +31,20 @@ function resetHerencia() {
     };
     xhr.send();
 }
+
+//Resetear Polimorfismo
+function resetPolimorfismo() {
+    var xhr = new XMLHttpRequest();
+    var serverMsg;
+
+    xhr.open("GET", "/debug/resetPolimorfismo.php", false);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            alert(JSON.stringify(json));
+        } else {
+            console.log("Error al cargar el servidor");
+        }
+    };
+    xhr.send();
+}
---
+++ debug/resetPolimorfismo.php
@@ -0,0 +1,51 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/php/config.php');
+
+// Recuperar datos del usuario
+$alumnoID = null;
+$alumnoUsername = null;
+
+if (isset($_COOKIE['userData'])) {
+    // Decodificar y deserializar la información de la cookie
+    $userDataJSONEncoded = $_COOKIE['userData'];
+    $userDataJSON = base64_decode($userDataJSONEncoded);
+    $userData = json_decode($userDataJSON, true); // true para obtener un array asociativo
+
+    // Acceder a 'username' e 'id' almacenados en la cookie
+    $alumnoID = $userData['id'];
+    $alumnoUsername = $userData['username'];
+} else {
+    header("Location: ../index.html");
+    exit();  // Asegúrate de salir del script después de la redirección
+}
+
+// Establecer la conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar si hay errores de conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Escapar y sanitizar las variables para evitar inyección SQL
+$alumnoID = $conn->real_escape_string($alumnoID);
+$alumnoUsername = $conn->real_escape_string($alumnoUsername);
+
+// Realizar la consulta SQL
+$sql = "UPDATE actividades_Polimorfismo
+        SET aciertos = NULL, actRealizada = 'No', fechaEntrega = '2023-10-15 18:46:53'
+        WHERE idAlumno = '$alumnoID' AND username = '$alumnoUsername'";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(['resultadoSQL' => 'Actividades de encapsulamiento reiniciadas para el usuario: ' . $alumnoUsername]); 
+} else {
+    $message = "Error al actualizar el registro: " . $conn->error;
+}
+
+// Cerrar la conexión
+$conn->close();
+
+// Redireccionar después de la operación de base de datos
+
+exit();  // Asegúrate de salir del script después de la redirección
+?>
