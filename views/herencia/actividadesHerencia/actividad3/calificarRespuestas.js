e4a25ff253bc00d333768d677e8741c427041070
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-22 23:55:35 -0600

    Actividad 3: Polimorfismo

--- views/herencia/actividadesHerencia/actividad3/calificarRespuestas.js
+++ views/herencia/actividadesHerencia/actividad3/calificarRespuestas.js
@@ -5,14 +5,9 @@ function calificarRespuestas() {
   let todosCamposCompletos = true;
 
   const respuestasCorrectas = {
-    q1: "b",
-    q2: "d",
-    q3: "b",
-    q4: "c",
-    q5: "c",
-    q6: "c",
-    q7: "c",
-    q8: "c",
+    q1: "c",
+    q2: "b",
+    q3: "b"
   };
 
   const numeroPreguntas = Object.keys(respuestasCorrectas).length;
