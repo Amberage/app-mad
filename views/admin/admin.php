66fc51b533aea76e57f4f27622cc6ce563f06c43
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-26 12:13:35 -0600

    Actualizacion

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -23,12 +23,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['crear_usuario'])) {
     $aPaterno = $_POST['aPaterno'];
     $aMaterno = $_POST['aMaterno'];
     $correo = $_POST['correo'];
-    $password = $_POST['password'];
+    $password = base64_encode($_POST['password']); // Codificar la contraseña en base64
     $userType = $_POST['userType'];
     $ip = $_SERVER['REMOTE_ADDR']; // Obtener la dirección IP del cliente
+    $fechaRegistro = date("Y-m-d H:i:s"); // Generar la fecha y hora automáticamente
 
-    $insertQuery = "INSERT INTO usuarios (username, nombre, aPaterno, aMaterno, correo, password, userType, ip)
-                    VALUES ('$username', '$nombre', '$aPaterno', '$aMaterno', '$correo', '$password', '$userType', '$ip')";
+    $insertQuery = "INSERT INTO usuarios (username, nombre, aPaterno, aMaterno, correo, password, userType, ip, fechaRegistro)
+                    VALUES ('$username', '$nombre', '$aPaterno', '$aMaterno', '$correo', '$password', '$userType', '$ip', '$fechaRegistro')";
 
     if ($conn->query($insertQuery) === TRUE) {
         echo "Registro creado con éxito.";
@@ -48,7 +49,7 @@ if (isset($_GET['edit_id'])) {
         $editedAPaterno = $_POST['editedAPaterno'];
         $editedAMaterno = $_POST['editedAMaterno'];
         $editedCorreo = $_POST['editedCorreo'];
-        $editedPassword = $_POST['editedPassword'];
+        $editedPassword = base64_encode($_POST['editedPassword']); // Codificar la contraseña en base64
         $editedUserType = $_POST['editedUserType'];
 
         $updateQuery = "UPDATE usuarios 
@@ -84,26 +85,10 @@ if (isset($_GET['delete_id'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" href="/assets/images/logo.png" />
+    <title>Usuarios</title>
 </head>
-<style>
-    body {
-        background-color: #ffffff;
-        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(90,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23FFEA8D'/%3E%3Cstop offset='1' stop-color='%23FF80FD'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(95,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23A36EFF'/%3E%3Cstop offset='1' stop-color='%2373FF60'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='3.3'%3E%3Cpath transform='translate(-9.1 -2.4000000000000004) rotate(-2.7 1409 581) scale(0.9779519999999999)' d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='1.1' transform='translate(-22 10) rotate(-1 800 450) scale(0.9969959999999999)' cx='500' cy='100' r='40'/%3E%3Cpath transform='translate(-3.8 6) rotate(-14 401 736) scale(0.9969959999999999)' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='1'%3E%3Cpath transform='translate(60 4) rotate(-1 150 345) scale(1.005984)' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='2.2' transform='translate(4 -29) rotate(-21.6 1089 759)' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='translate(-15.2 0.7999999999999998) rotate(-3.5999999999999996 1400 132) scale(0.96)' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-        background-attachment: fixed;
-        background-size: cover;
-        margin: 0;
-        padding: 0;
-        width: 100%;
-    }
-</style>
 <body>
     <h1>Lista de Usuarios</h1>
     <table border="1">
@@ -129,7 +114,7 @@ if (isset($_GET['delete_id'])) {
             echo "<td>" . $row['aPaterno'] . "</td>";
             echo "<td>" . $row['aMaterno'] . "</td>";
             echo "<td>" . $row['correo'] . "</td>";
-            echo "<td>" . $row['password'] . "</td>";
+            echo "<td>" . base64_decode($row['password']) . "</td>"; // Decodificar la contraseña en base64
             echo "<td>" . $row['userType'] . "</td>";
             echo "<td>" . $row['fechaRegistro'] . "</td>";
             echo "<td>" . $row['ip'] . "</td>";
@@ -155,7 +140,7 @@ if (isset($_GET['delete_id'])) {
             <label>Correo:</label>
             <input type="email" name="editedCorreo" value="<?php echo $editData['correo']; ?>" required><br>
             <label>Password:</label>
-            <input type="password" name="editedPassword" value="<?php echo $editData['password']; ?>" required><br>
+            <input type="password" name="editedPassword" value="<?php echo base64_decode($editData['password']); ?>" required><br>
             <label>Tipo de Usuario:</label>
             <select name="editedUserType">
                 <option value="alumno" <?php if ($editData['userType'] == 'alumno') echo 'selected'; ?>>Alumno</option>
