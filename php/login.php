09853e5d8041ea1f99be0d4442f0f11e83ab265d
Alejandro Galicia [Workstation] <amberage.exe@gmail.com>
2023-10-26 20:12:54 -0600

    Cambios menores

--- php/login.php
+++ php/login.php
@@ -19,43 +19,40 @@ if ($result->num_rows > 0) {
     $hashedPassword = $row['password'];
     $tipoCuenta = $row['userType'];
 
-    // Obtén la IP del usuario
-    $ip = $_SERVER['REMOTE_ADDR'];
+    if (password_verify($password, $hashedPassword)) {
+        $ip = $_SERVER['REMOTE_ADDR'];
 
-    // Suponiendo que $userData es un array con los datos del usuario que quieres almacenar en la cookie
-    $userData = array(
-        "id" => $row['id'],
-        "username" => $row['username'],
-        "nombre" => $row['nombre'],
-        "aPaterno" => $row['aPaterno'],
-        "aMaterno" => $row['aMaterno'],
-        "correo" => $row['correo'],
-        "userType" => $row['userType'],
-        "ip" => $ip,
-        "fechaRegistro" => $row['fechaRegistro']
-    );
+        $userData = array(
+            "id" => $row['id'],
+            "username" => $row['username'],
+            "nombre" => $row['nombre'],
+            "aPaterno" => $row['aPaterno'],
+            "aMaterno" => $row['aMaterno'],
+            "correo" => $row['correo'],
+            "userType" => $row['userType'],
+            "ip" => $ip,
+            "fechaRegistro" => $row['fechaRegistro']
+        );
 
-    // Convertir el array a JSON y codificar para que sea seguro para las cookies
-    $userDataJSON = json_encode($userData);
-    $userDataJSONEncoded = base64_encode($userDataJSON);
+        $userDataJSON = json_encode($userData);
+        $userDataJSONEncoded = base64_encode($userDataJSON);
 
-    // Establecer la cookie con la información del usuario
-    setcookie("userData", $userDataJSONEncoded, time() + (3600), "/"); 
+        $redirectPath = "";
 
-    // Verificar la contraseña proporcionada con la contraseña encriptada
-    if (password_verify($password, $hashedPassword) && $tipoCuenta == 'alumno') {
-        // Contraseña válida, inicio de sesión exitoso
-        echo json_encode(array("error" => false, "message" => "Inicio de sesión exitoso", "redirect" => "home.html"));
-    } else if (password_verify($password, $hashedPassword) && $tipoCuenta == 'docente'){
-        echo json_encode(array("error" => false, "message" => "Inicio de sesión exitoso", "redirect" => "/views/admin/docente.html"));
-    } else if (password_verify($password, $hashedPassword) && $tipoCuenta == 'administrador'){
-        echo json_encode(array("error" => false, "message" => "Inicio de sesión exitoso", "redirect" => "/views/admin/admin.php"));
+        if ($tipoCuenta == 'alumno') {
+            $redirectPath = "home.html";
+        } elseif ($tipoCuenta == 'docente') {
+            $redirectPath = "/views/admin/docente.html";
+        } elseif ($tipoCuenta == 'administrador') {
+            $redirectPath = "/views/admin/admin.php";
+        }
+
+        setcookie("userData", $userDataJSONEncoded, time() + (3600), "/");
+        echo json_encode(array("error" => false, "message" => "Inicio de sesión exitoso", "redirect" => $redirectPath));
     } else {
-        // Contraseña incorrecta
         echo json_encode(array("error" => true, "message" => "Contraseña Incorrecta", "redirect" => "/views/login.html"));
     }
 } else {
-    // Usuario no encontrado
     setcookie("userData", "", time() - 3600, "/");
     echo json_encode(array("error" => true, "message" => "Usuario inexistente"));
 }
