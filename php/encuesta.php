9e9e779dfdfa8e8b1a3bec2ea433b9f72b881733
MarianaVSilva <marianavazquezsilva@gmail.com>
2023-11-11 00:12:32 -0600

    Encuesta de Satisfacción (php, html)

--- php/encuesta.php
+++ php/encuesta.php
@@ -1,26 +1,39 @@
 <?php
 include 'config.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+//Verifica si se recibieron los datos
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoger los valores del formulario
+    $idAlumno = $_SESSION['id_usuario']; 
+    $username = $_SESSION['username']; 
+    $pregunta1 = $_POST['pregunta1'];
+    $pregunta2 = $_POST['pregunta2'];
+    $pregunta3 = $_POST['pregunta3'];
+    $pregunta4 = $_POST['pregunta4'];
+    $comentarios = $_POST['comentarios'];
 
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+    //Conecta a la base de datos
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-$pregunta1 = $_POST["pregunta1"];
-$pregunta2 = $_POST["pregunta2"];
-$pregunta3 = $_POST["pregunta3"];
-$pregunta4 = $_POST["pregunta4"];
-$comentarios = $_POST["comentarios"];
+    //Verifica la conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
 
-$sql = "INSERT INTO encuesta_satisfacción (pregunta1, pregunta2, pregunta3, pregunta4, comentarios)
-        VALUES ('$pregunta1', '$pregunta2', '$pregunta3', '$pregunta4', '$comentarios')";
+    //Prepara la consulta SQL para insertar en la tabla encuesta_satisfaccion
+    $sql = "INSERT INTO encuesta_satisfaccion (idAlumno, username, pregunta1, pregunta2, pregunta3, pregunta4, encuestaContestada)
+            VALUES ('$idAlumno', '$username', '$pregunta1', '$pregunta2', '$pregunta3', '$pregunta4', 'Si')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Respuestas guardadas correctamente.";
+    // Ejecutar la consulta y verificar si se insertaron los datos
+    if ($conn->query($sql) === TRUE) {
+        echo "Encuesta almacenada correctamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Cerrar la conexión a la base de datos
+    $conn->close();
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "No se han recibido datos del formulario";
 }
-
-$conn->close();
 ?>
