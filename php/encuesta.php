acf1726355b39ad87936ea65633189de7bcca603
MarianaVSilva <marianavazquezsilva@gmail.com>
2023-11-12 01:55:54 -0600

    Encuesta (BD)

--- php/encuesta.php
+++ php/encuesta.php
@@ -1,16 +1,14 @@
 <?php
 include 'config.php';
 
-//Verifica si se recibieron los datos
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoger los valores del formulario
-    $idAlumno = $_SESSION['id_usuario']; 
-    $username = $_SESSION['username']; 
-    $pregunta1 = $_POST['pregunta1'];
-    $pregunta2 = $_POST['pregunta2'];
-    $pregunta3 = $_POST['pregunta3'];
-    $pregunta4 = $_POST['pregunta4'];
-    $comentarios = $_POST['comentarios'];
+session_start(); // Inicia la sesión si aún no está iniciada
+
+// Verifica si el usuario tiene la sesión iniciada
+if (!isset($_SESSION['id'])) {
+    // Redirige a la página de inicio de sesión si el usuario no tiene sesión iniciada
+    header("Location: iniciar_sesion.php");
+    exit;
+}
 
     //Conecta a la base de datos
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,20 +18,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Conexión fallida: " . $conn->connect_error);
     }
 
+    $idAlumno = $_SESSION['id_usuario']; 
+    $username = $_SESSION['username']; 
+    $pregunta1 = $_POST['pregunta1'];
+    $pregunta2 = $_POST['pregunta2'];
+    $pregunta3 = $_POST['pregunta3'];
+    $pregunta4 = $_POST['pregunta4'];
+    $comentarios = $_POST['comentarios'];
+
     //Prepara la consulta SQL para insertar en la tabla encuesta_satisfaccion
     $sql = "INSERT INTO encuesta_satisfaccion (idAlumno, username, pregunta1, pregunta2, pregunta3, pregunta4, encuestaContestada)
             VALUES ('$idAlumno', '$username', '$pregunta1', '$pregunta2', '$pregunta3', '$pregunta4', 'Si')";
 
     // Ejecutar la consulta y verificar si se insertaron los datos
     if ($conn->query($sql) === TRUE) {
-        echo "Encuesta almacenada correctamente";
+        echo "Encuesta enviada correctamente";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error al enviar la encuesta: " . $conn->error;
     }
-
-    // Cerrar la conexión a la base de datos
+    
     $conn->close();
-} else {
-    echo "No se han recibido datos del formulario";
-}
-?>
+    ?>
